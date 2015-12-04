c36162c08ad7cc4f543bdb732130ff5a2e73101a
Mike Litman <mike@awesomefat.com>
2015-12-04 15:02:27 -0600

    -initial commit

--- Distress/ViewController.swift
+++ Distress/ViewController.swift
@@ -8,11 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate
+{
 
-    override func viewDidLoad() {
+    var count = 0
+    
+    @IBOutlet weak var theCV: UICollectionView!
+      
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
+    {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        // #warning Incomplete implementation, return the number of items
+        return count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CustomCVCell
+        
+        // Configure the cell
+        cell.backgroundColor = UIColor.redColor()
+        cell.theLabel.text = "blah"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
